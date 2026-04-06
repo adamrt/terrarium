28a1febd0e6cfd1636548ae9113ed2f06f5e4e59
Adam Patterson <adam@adamrt.com>
2026-04-06 16:37:47 -0500

    Add gfx_surface_blit

--- src/gfx/draw.c
+++ src/gfx/draw.c
@@ -1,10 +1,13 @@
 #include "gfx/draw.h"
+#include "ak/assert.h"
 #include "ak/types.h"
 #include "gfx/color.h"
 #include "gfx/surface.h"
 
 void gfx_draw_pixel(gfx_surface_t* surface, i32 x, i32 y, gfx_color_t color)
 {
+    ASSERT(surface);
+
     gfx_pixel_t packed = gfx_color_pack(color);
     surface->data[y * surface->width + x] = packed;
 }
--- src/gfx/surface.c
+++ src/gfx/surface.c
@@ -41,3 +41,25 @@ void gfx_surface_clear(gfx_surface_t* surface, gfx_color_t color)
         }
     }
 }
+
+void gfx_surface_blit(gfx_surface_t* target, const gfx_surface_t* source, i32 x, i32 y)
+{
+    i32 dx_start = x;
+    i32 dy_start = y;
+    i32 dx_end = dx_start + source->width;
+    i32 dy_end = dy_start + source->height;
+
+    i32 sx = 0;
+    i32 sy = 0;
+
+    for (i32 dy = dy_start; dy < dy_end; ++dy) {
+        for (i32 dx = dx_start; dx < dx_end; ++dx) {
+            i32 d_index = dy * target->width + dx;
+            i32 s_index = sy * source->width + sx;
+            target->data[d_index] = source->data[s_index];
+            sx++;
+        }
+        sy++;
+        sx = 0;
+    }
+}
