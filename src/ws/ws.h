d7f6923144219c2c3f1e490bb90c9eeeebc1e3ff
Adam Patterson <adam@adamrt.com>
2026-04-06 19:50:41 -0500

    Change ws_window_t to be public

--- src/ws/ws.h
+++ src/ws/ws.h
@@ -7,15 +7,11 @@
 // Window
 //
 
-typedef struct ws_window ws_window_t;
-
-// FIXME: Move this into .c
-struct ws_window {
+typedef struct {
     gfx_rect_t rect;
     gfx_color_t content_color;
-
     gfx_surface_t* content;
-};
+} ws_window_t;
 
 ws_window_t* ws_window_create(mem_allocator_t* alloc, i32 x, i32 y, i32 width, i32 height);
 void ws_window_destroy(mem_allocator_t* alloc, ws_window_t* window);
