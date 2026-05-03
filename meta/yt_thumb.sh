7ec11bfe736c39335bcc522e042cca0b4c7c881d
Adam Patterson <adam@adamrt.com>
2026-05-02 17:17:40 -0700

    Reformat yt_thumb.sh

--- meta/yt_thumb.sh
+++ meta/yt_thumb.sh
@@ -15,13 +15,17 @@ out="thumb_ep_${episode}.png"
 font="/usr/local/share/fonts/b/BerkeleyMono_Regular.ttf"
 
 char_count=${#title}
-if   [[ $char_count -le 15 ]]; then pointsize=96
-elif [[ $char_count -le 22 ]]; then pointsize=80
-elif [[ $char_count -le 30 ]]; then pointsize=64
-else                                pointsize=52
+if [[ $char_count -le 15 ]]; then
+    pointsize=96
+elif [[ $char_count -le 22 ]]; then
+    pointsize=80
+elif [[ $char_count -le 30 ]]; then
+    pointsize=64
+else
+    pointsize=52
 fi
 
-title_offset=$(( pointsize / 2 + 28 ))
+title_offset=$((pointsize / 2 + 28))
 
 cmd=(
     magick
